e2a7c2660dc3842df12ba27f7fe3086645338004
sergeysavin <serzz1990@gmail.com>
2026-01-06 17:00:53 +0300

    Update index.html title and adjust script path; modify vite.config.js for dynamic base path based on environment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/loto/',
+  base: process.env.NODE_ENV === 'production' ? '/loto/' : '/',
 })
